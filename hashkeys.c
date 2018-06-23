889b5e589af0ae156df94faf92b58a535b353682
Bhavya Rawal <koolbhavya.epic@gmail.com>
2018-06-23 14:48:02 +0530

    Update hashkeys.c

--- hashkeys.c
+++ hashkeys.c
@@ -10,19 +10,23 @@ U64 GeneratePosKey(const S_BOARD *pos)
 	int piece = EMPTY;
 	
 	// pieces
-	for(sq = 0; sq < BRD_SQ_NUM; ++sq) {
+	for(sq = 0; sq < BRD_SQ_NUM; ++sq)
+	{
 		piece = pos->pieces[sq];
-		if(piece!=NO_SQ && piece!=EMPTY &&piece!=OFFBOARD) {
+		if(piece!=NO_SQ && piece!=EMPTY &&piece!=OFFBOARD)
+		{
 			ASSERT(piece>=wP && piece<=bK);
 			finalKey ^= PieceKeys[piece][sq];
 		}		
 	}
 	
-	if(pos->side == WHITE) {
+	if(pos->side == WHITE)
+	{
 		finalKey ^= SideKey;
 	}
 		
-	if(pos->enPas != NO_SQ) {
+	if(pos->enPas != NO_SQ)
+	{
 		ASSERT(pos->enPas>=0 && pos->enPas<BRD_SQ_NUM);
 		finalKey ^= PieceKeys[EMPTY][pos->enPas];
 	}
