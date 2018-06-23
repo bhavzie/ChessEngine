c15b72a99091bf28ad6c8f92086ddc7e8397d320
Bhavya Rawal <koolbhavya.epic@gmail.com>
2018-06-23 14:45:03 +0530

    Update defs.h

--- defs.h
+++ defs.h
@@ -182,7 +182,9 @@
 	//attack.c	
 	extern int SqAttacked(const int sq,const int side,const S_BOARD *pos);
 
-
+	// io.c
+	extern char *PrSq(const int sq);
+	extern char *PrMove(const int move);
 
 
 #endif
