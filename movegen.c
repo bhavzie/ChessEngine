482a447d5d3eb9259044032c91c75017f79af6e1
Bhavya Rawal <koolbhavya.epic@gmail.com>
2018-06-26 19:49:25 +0530

    Update movegen.c

--- movegen.c
+++ movegen.c
@@ -55,6 +55,36 @@ void AddWhitePawnMove(const S_BOARD *pos,const int from,const int to,S_MOVELIST 
 	}
 }
 
+void AddBlackPawnCapMove(const S_BOARD *pos,const int from,const int to,const int cap,S_MOVELIST *list)
+{
+	if(RanksBrd[from]==RANK_2)
+	{
+		AddCaptureMove(pos,MOVE(from,to,cap,bQ,0),list);
+		AddCaptureMove(pos,MOVE(from,to,cap,bR,0),list);
+		AddCaptureMove(pos,MOVE(from,to,cap,bB,0),list);
+		AddCaptureMove(pos,MOVE(from,to,cap,bN,0),list);		
+	}
+	else
+	{
+		AddCaptureMove(pos,MOVE(from,to,cap,EMPTY,0),list);	// Say its not on the 7th rank so it cannot promote to anything
+	}
+}
+
+void AddBlackPawnMove(const S_BOARD *pos,const int from,const int to,S_MOVELIST *list)
+{
+	if(RanksBrd[from]==RANK_2)
+	{
+		AddQuietMove(pos,MOVE(from,to,EMPTY,bQ,0),list);
+		AddQuietMove(pos,MOVE(from,to,EMPTY,bR,0),list);
+		AddQuietMove(pos,MOVE(from,to,EMPTY,bB,0),list);
+		AddQuietMove(pos,MOVE(from,to,EMPTY,bN,0),list);		
+	}
+	else
+	{
+		AddQuietMove(pos,MOVE(from,to,EMPTY,EMPTY,0),list);
+	}
+}
+
 void GenerateAllMoves(const S_BOARD *pos,S_MOVELIST *list)
 {
 
@@ -102,6 +132,37 @@ void GenerateAllMoves(const S_BOARD *pos,S_MOVELIST *list)
 	}
 	else
 	{
-		
+		for(pceNum=0;pceNum<pos->pceNum[bP];++pceNum)
+		{
+			sq=pos->pList[bP][pceNum];
+			ASSERT(SqOnBoard(sq));
+			
+		if(pos->pieces[sq-10]==EMPTY)
+		{
+			AddBlackPawnMove(pos,sq,sq-10,list);
+			if(RanksBrd[sq]==RANK_7 && pos->pieces[sq-20]==EMPTY)
+			{
+				AddQuietMove(pos,MOVE(sq,(sq-20),EMPTY,EMPTY,MFLAGPS),list);
+			}
+				
+		}
+		if(!SQOFFBOARD(sq-9)&&PieceCol[pos->pieces[sq-9]]==WHITE)
+		{
+			AddBlackPawnCapMove(pos,sq,sq-9,pos->pieces[sq-9],list);
+		}
+		if(!SQOFFBOARD(sq-11)&&PieceCol[pos->pieces[sq-11]]==WHITE)
+		{
+			AddBlackPawnCapMove(pos,sq,sq-11,pos->pieces[sq-11],list);
+		}	
+		}
+		if(sq-9==pos->enPas)
+		{
+			AddCaptureMove(pos,MOVE(sq,sq-9,EMPTY,EMPTY,MFLAGEP),list);
+		}
+		if(sq-11==pos->enPas)
+		{
+			AddCaptureMove(pos,MOVE(sq,sq-11,EMPTY,EMPTY,MFLAGEP),list);
+		}
 	}
+	
 }
