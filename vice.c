65c359f4aab0214348305510c8e9b8ef0831616e
Bhavya Rawal <koolbhavya.epic@gmail.com>
2018-06-28 11:57:02 +0530

    Update vice.c

--- vice.c
+++ vice.c
@@ -1,21 +1,22 @@
-#include<stdio.h>
-#include"defs.h"
-#include<stdlib.h>
+#include "stdio.h"
+#include "defs.h"
+#include "stdlib.h"
 
+#define CASTLE1 "r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1"
+#define CASTLE2 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
 
-#define FEN4 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
-int main()
-{
+int main() {	
 
-	AllInit();	
+	AllInit();		
 	
 	S_BOARD board[1];
-	ParseFen(FEN4,board);
-	PrintBoard(board);
-	ASSERT(CheckBoard(board));
+	S_MOVELIST list[1];
 	
+	ParseFen(CASTLE2,board);
+	GenerateAllMoves(board,list);
 	
-	
+	PrintMoveList(list);
 	
 	return 0;
 }
+
