02bf3c0b8eb7787d04c2c32332cf0862f42e2185
Bhavya Rawal <koolbhavya.epic@gmail.com>
2018-06-28 11:54:30 +0530

    Update defs.h

--- defs.h
+++ defs.h
@@ -189,7 +189,7 @@ typedef struct {
 	// board.c
         extern void ResetBoard(S_BOARD *pos);
 	extern int ParseFen(char *fen,S_BOARD *pos);
-	extern void PrintBoard(S_BOARD *pos);
+	extern void PrintBoard(const S_BOARD *pos);
 	extern void UpdateListsMaterial(S_BOARD *pos);
 	extern int CheckBoard(const S_BOARD *pos);
 	
