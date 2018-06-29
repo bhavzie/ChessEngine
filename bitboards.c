3f4eb4b861f095d59614f31e4a66b8ed1b2141d3
Bhavya Rawal <koolbhavya.epic@gmail.com>
2018-06-29 10:46:01 +0530

    Update bitboards.c

--- bitboards.c
+++ bitboards.c
@@ -8,20 +8,23 @@ const int BitTable[64] = {
   58, 20, 37, 17, 36, 8
 };
 
-int PopBit(U64 *bb) {
+int PopBit(U64 *bb)
+{
   U64 b = *bb ^ (*bb - 1);
   unsigned int fold = (unsigned) ((b & 0xffffffff) ^ (b >> 32));
   *bb &= (*bb - 1);
   return BitTable[(fold * 0x783a9b23) >> 26];
 }
 
-int CountBits(U64 b) {
+int CountBits(U64 b)
+{
   int r;
   for(r = 0; b; r++, b &= b - 1);
   return r;
 }
 
-void PrintBitBoard(U64 bb) {
+void PrintBitBoard(U64 bb)
+{
 
 	U64 shiftMe = 1ULL;
 	
@@ -31,8 +34,10 @@ void PrintBitBoard(U64 bb) {
 	int sq64 = 0;
 	
 	printf("\n");
-	for(rank = RANK_8; rank >= RANK_1; --rank) {
-		for(file = FILE_A; file <= FILE_H; ++file) {
+	for(rank = RANK_8; rank >= RANK_1; --rank)
+	{
+		for(file = FILE_A; file <= FILE_H; ++file)
+		{
 			sq = FR2SQ(file,rank);	// 120 based		
 			sq64 = SQ64(sq); // 64 based
 			
